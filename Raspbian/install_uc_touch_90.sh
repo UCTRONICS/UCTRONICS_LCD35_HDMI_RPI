5eef58108974db4b29035facecccae86d2a29617
digiwhale <digiwhale.humphrey@gmail.com>
2022-05-30 01:25:26 -0400

    1

--- Raspbian/install_uc_touch_90.sh
+++ Raspbian/install_uc_touch_90.sh
@@ -11,7 +11,7 @@ sudo echo "hdmi_mode=1" >> ./boot/config.txt.bak
 sudo echo "hdmi_mode=87" >> ./boot/config.txt.bak
 sudo echo "hdmi_cvt 480 320 60 6 0 0 0" >> ./boot/config.txt.bak
 sudo echo "hdmi_drive=2" >> ./boot/config.txt.bak
-sudo echo "display_rotate=3" >> ./boot/config.txt.bak
+sudo echo "display_rotate=1" >> ./boot/config.txt.bak
 sudo cp -rf ./boot/config.txt.bak /boot/config.txt
 sleep 1
 echo "reboot now"
